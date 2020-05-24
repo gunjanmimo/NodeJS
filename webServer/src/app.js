dcacc6300be8b1a72d00fc4387599b1b03001690
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-05-24 18:18:26 +0530

    new

--- webServer/src/app.js
+++ webServer/src/app.js
@@ -1,17 +1,21 @@
+const path = require("path")
 const express = require("express")
-
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
 
 const app = express()
+const publicDirectory=path.join(__dirname,'../public')
+
+app.use(express.static(publicDirectory))
 
 
-//app.com
-//app.com/help
-//app.com/about
 
-app.get('',(req, res)=>{
-    res.send("hi i am using nodejs")
+app.get('/weather', (req, res) => {
+    res.send({
+        location: "jaipur",
+        temp: "30 C"
+    })
 })
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on port: 3000")
 })
