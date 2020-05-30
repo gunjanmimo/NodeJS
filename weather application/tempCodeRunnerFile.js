ada46ce563c8735590786b118c22b0e8e50bbc9e
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-05-30 10:22:01 +0530

    new

--- weather application/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-const data=response.body;
-  // console.log("Weather: "+data.main.temp+" C")
-  // console.log("Rain forcast: "+data.clouds.all)
--- weather_news/public/js/app.js
+++
@@ -1,5 +0,0 @@
-console.log("testing")
-const weatherForm= document.querySelector('form')
-weatherForm.addEventListener('submit',()=>{
-    
-})
