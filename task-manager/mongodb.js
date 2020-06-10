a2c367b2ed676cddf9ace68b013a6118e8b330bf
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-06-10 14:20:27 +0530

    new

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,6 +1,5 @@
 //crud= reate read update delete
 
-
 const mongodb = require("mongodb")
 const MongoClient = mongodb.MongoClient
 
@@ -14,9 +13,29 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: "Gunjan",
-        age: 20
+    // db.collection('users').insertOne({
+    //     name: "Mimo",
+    //     age: 20
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log("unable to insert data")
+    //     }
+    //     console.log(result.ops)
+
+    // })
+    db.collection('user').insertMany([
+        {
+            name: 'jen',
+            age: 29
+        }, {
+            name: "Mokos",
+            age: 20
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log("unable to insert document")
+        }
+        console.log(result.ops)
     })
 
 })
