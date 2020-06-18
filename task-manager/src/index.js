917155e7e0fa14247d601c1221c8956c97f3b0df
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-06-18 13:22:24 +0530

    new

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,7 +4,16 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+
+app.use(express.json())
+
+app.post("/users",(req,res)=>{
+    console.log(req.body)
+    res.send("testing")
+})
+
+
 app.listen(port, () => {
     console.log("srver is runnning on port: ", port)
-    console.log("localhost:", port)
+    console.log("http://localhost:"+port)
 })
