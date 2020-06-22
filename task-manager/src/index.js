fd212484d3f4eb068a4b9ee90631f3759a49e0a1
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-06-22 12:05:17 +0530

    new

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 require("./db/mangoose")
 const User = require("./models/user")
+const Task = require("./models/tasks")
 
 
 const app = express()
@@ -9,17 +10,35 @@ const port = process.env.PORT || 3000
 
 
 app.use(express.json())
-
+//creating resounces
 app.post("/users", (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
 
     })
 })
 
+
+app.get("/users", (res, req) => {
+    User.find({})
+})
+
+
+
+
+
+
+app.post("/tasks", (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
 
 app.listen(port, () => {
     console.log("srver is runnning on port: ", port)
