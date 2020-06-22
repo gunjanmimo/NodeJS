4306fbbb887a6c297fa47ae55003171c905600b2
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-06-22 14:10:48 +0530

    new

---
+++ task-manager/src/models/playground/promise-chaining.js
@@ -0,0 +1,15 @@
+require('../src/db/mangoose')
+const User = require("../src/models/user")
+
+//5ee9c5738275dddfdc0a6bab
+
+User.findByIdAndUpdate('5ee9c5738275dddfdc0a6bab', {
+    age: 98
+}).then((user) => {
+    console.log(user)
+    return User.countDocuments({ age: 20 })
+}).then((result) => {
+    console.log(result)
+}).catch((error) => {
+    console.log(error)
+})
