f3f3283ec52b189b4dff2aa28f80344273a61cd5
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-06-23 11:39:06 +0530

    new

--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -2,9 +2,11 @@ require('../src/db/mangoose')
 const Task = require("../src/models/tasks")
 
 
-Task.findByIdAndRemove('5ee623603938377b57c90e88').then(() => {
-    console.log("removed")
-    return Task.countDocuments({ completed: false })
+Task.findByIdAndRemove('5ee623603938377b57c90e88').then((task) => {
+    console.log(task)
+    return Task.countDocuments()
+}).then((result) => {
+    console.log(result)
 }).catch((e) => {
     console.log(e)
 })
