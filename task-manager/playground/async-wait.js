2203f7d87a7e4817df943f6fd42b72fe9892493e
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-06-23 14:09:34 +0530

    new

--- task-manager/playground/async-wait.js
+++ task-manager/playground/async-wait.js
@@ -1,6 +1,21 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject("nuber must be non negative")
+            }
+            resolve(a + b)
+
+        }, 20)
+    })
+}
+
+
 const doWork = async () => {
-    throw new Error("went wrong")
-    // return "Gunjan"
+    const sum1 = await add(-1, 100)
+    const sum2 = await add(sum1, 100)
+    const sum3 = await add(sum2, -100)
+    return sum3
 }
 
 doWork().then((result) => {
